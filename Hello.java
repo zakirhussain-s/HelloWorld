fedf0ae8225bdccee0a35f906501f3b8043b71ad
Zakir Hussain <s.zakirhussain02@gmail.com>
2021-02-28 13:19:08 +0530

    changes added

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		for(int i=1;i<10;i++){
-			System.out.println("Hello World times "+i+" times times");
+			System.out.println("Hello World times "+i+" no times");
 }
 }
 }
